c22d1dedd09b6c83c7014217ab2ac06693ca520d
maconrm180 <maconrm180@DESKTOP-F9U5424>
2020-10-22 10:36:03 -0500

    For a class lesson

---
+++ src/myfirsthelloworld/Myfirsthelloworld.java
@@ -0,0 +1,31 @@
+
+package myfirsthelloworld;
+
+
+
+public class Myfirsthelloworld {
+// My Hello World - Jamarius Oct 2020
+ 
+    public static void main(String[] args) { 
+        
+        
+    System.out.println("Hello to you world ");
+    
+
+    
+        
+        
+       
+        
+        
+        
+        
+        
+      
+        
+        
+        
+        
+    }
+    
+}
